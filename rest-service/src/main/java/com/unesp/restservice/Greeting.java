a30b1d0be4f962882f2ec3f01c7008b0e02c9400
MateusFlores <mateusgf.42@gmail.com>
2023-07-05 12:20:47 -0300

    Finished my first rest service from spring tutorials

---
+++ rest-service/src/main/java/com/unesp/restservice/Greeting.java
@@ -0,0 +1,4 @@
+package com.unesp.restservice;
+
+public record Greeting(long id, String content) {
+}
--- rest-service/src/main/java/com/unesp/restservice/RestServiceApplication.java
+++ rest-service/src/main/java/com/unesp/restservice/RestServiceApplication.java
@@ -2,12 +2,14 @@ package com.unesp.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.reactive.context.GenericReactiveWebApplicationContext;
 
 @SpringBootApplication
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
+
 	}
 
 }
