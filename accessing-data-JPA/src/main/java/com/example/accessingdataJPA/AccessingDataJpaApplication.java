85938ec86656adaea40992a71880b39906bbe6c1
MateusFlores <mateusgf.42@gmail.com>
2023-07-12 15:12:39 -0300

    add accessing-data-JPA project from Spring initializr

---
+++ accessing-data-JPA/src/main/java/com/example/accessingdataJPA/AccessingDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessingdataJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataJpaApplication.class, args);
+	}
+
+}
---
+++ accessing-data-JPA/src/test/java/com/example/accessingdataJPA/AccessingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingdataJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
