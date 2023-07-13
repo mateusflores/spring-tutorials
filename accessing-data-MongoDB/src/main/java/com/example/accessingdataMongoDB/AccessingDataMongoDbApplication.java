399070b6f958cb6ff96fc51d0703e1297269e8dc
MateusFlores <mateusgf.42@gmail.com>
2023-07-13 10:01:13 -0300

    add project accessing-data-MongoDB from spring initializr

---
+++ accessing-data-MongoDB/src/main/java/com/example/accessingdataMongoDB/AccessingDataMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessingdataMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataMongoDbApplication.class, args);
+	}
+
+}
---
+++ accessing-data-MongoDB/src/test/java/com/example/accessingdataMongoDB/AccessingDataMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingdataMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
