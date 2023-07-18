d3dec5dc3796effa5ff7b085d3724195f27ef5c0
MateusFlores <mateusgf.42@gmail.com>
2023-07-18 09:30:34 -0300

    add AccessingJPADataWithREST project from spring initializr

---
+++ AccessingJPADataWithREST/src/main/java/com/example/AccessingJPADataWithREST/AccessingJpaDataWithRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.AccessingJPADataWithREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingJpaDataWithRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingJpaDataWithRestApplication.class, args);
+	}
+
+}
---
+++ AccessingJPADataWithREST/src/test/java/com/example/AccessingJPADataWithREST/AccessingJpaDataWithRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AccessingJPADataWithREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingJpaDataWithRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
