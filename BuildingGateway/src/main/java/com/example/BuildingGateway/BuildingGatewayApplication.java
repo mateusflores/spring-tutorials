e878b035334764937e2a31d5928ad52049318767
MateusFlores <mateusgf.42@gmail.com>
2023-07-24 10:28:49 -0300

    add BuildingGateway project from spring initializr

---
+++ BuildingGateway/src/main/java/com/example/BuildingGateway/BuildingGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.BuildingGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingGatewayApplication.class, args);
+	}
+
+}
---
+++ BuildingGateway/src/test/java/com/example/BuildingGateway/BuildingGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BuildingGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
